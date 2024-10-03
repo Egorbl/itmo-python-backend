f2034e58f4755179b2c79c82a2577920c53bf26c
Egorbl <egor.belyakovvvv@gmail.com>
2024-10-03 17:01:41 +0300

    добавил hw1 в новую репу

--- lecture_1/hw/math_plain_asgi.py
+++ lecture_1/hw/math_plain_asgi.py
@@ -1,8 +1,28 @@
-from typing import Any, Awaitable, Callable
+from lecture_1.hw.helper.helper import send_answer
+from lecture_1.hw.src.factorial import factorial
+from lecture_1.hw.src.fibonacci import fibonacci
+from lecture_1.hw.src.mean import mean
 
+async def app(scope, receive, send) -> None:
+    if scope["type"] == "http":
+        path = scope['path']
 
-async def app(
-    scope: dict[str, Any],
-    receive: Callable[[], Awaitable[dict[str, Any]]],
-    send: Callable[[dict[str, Any]], Awaitable[None]],
-) -> None: ...
+        if path == "/factorial":
+            await factorial(scope, receive, send)
+            return
+        if path.startswith("/fibonacci"):
+            await fibonacci(scope, receive, send)
+            return
+        if path == "/mean":
+            await mean(scope, receive, send)
+            return
+
+        await send_answer(send, 404, "404 Not Found")
+    elif scope["type"] == "lifespan":
+        while True:
+            message = await receive()
+            if message["type"] == "lifespan.startup":
+                await send({"type": "lifespan.startup.complete"})
+            elif message["type"] == "lifespan.shutdown":
+                await send({"type": "lifespan.shutdown.complete"})
+                return
--- tests/test_homework_1.py
+++ tests/test_homework_1.py
@@ -7,7 +7,6 @@ from async_asgi_testclient import TestClient
 from lecture_1.hw.math_plain_asgi import app
 
 
-@pytest.mark.xfail()
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     ("method", "path"),
@@ -20,6 +19,8 @@ from lecture_1.hw.math_plain_asgi import app
 )
 async def test_not_found(method: str, path: str):
     async with TestClient(app) as client:
+        print("КЛИЕНТ")
+        print(client)
         response = await client.open(
             path,
             method=method,
@@ -27,7 +28,6 @@ async def test_not_found(method: str, path: str):
         assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-@pytest.mark.xfail()
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     ("query", "status_code"),
@@ -51,7 +51,6 @@ async def test_factorial(query: dict[str, Any], status_code: int):
         assert "result" in response.json()
 
 
-@pytest.mark.xfail()
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     ("params", "status_code"),
@@ -72,7 +71,6 @@ async def test_fibonacci(params: str, status_code: int):
         assert "result" in response.json()
 
 
-@pytest.mark.xfail()
 @pytest.mark.asyncio()
 @pytest.mark.parametrize(
     ("json", "status_code"),
